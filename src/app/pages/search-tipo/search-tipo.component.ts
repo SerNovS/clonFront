aff6d44ad3276a7a78da80112a966c834eecea1d
FelipeCastillo1 <felipe.castillocontreras@gmail.com>
2022-11-24 00:21:51 -0300

    created component search tipo

---
+++ src/app/pages/search-tipo/search-tipo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'search-tipo',
+  templateUrl: './search-tipo.component.html'
+})
+export class SearchTipoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
